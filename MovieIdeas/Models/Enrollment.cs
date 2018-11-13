f6eb0e8a2011665e111ab3ca9b2ece2ca05c863f
ADS\yli425 <yuan.li@aexp.com>
2018-11-13 13:50:52 -0700

    create the data model

---
+++ MovieIdeas/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieIdeas.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int TheaterID { get; set; }
+        public int MovieID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Theater Theater { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
---
+++ MovieIdeas/Models/Theater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MovieIdeas.Models
+{
+    public class Theater
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TheaterID { get; set; }
+        public string Title { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
