bf845792919ff987cf4b129af39ebb6502ce2c9d
ADS\yli425 <yuan.li@aexp.com>
2018-11-13 14:00:11 -0700

    Create the database context

---
+++ MovieIdeas/DAL/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using MovieIdeas.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MovieIdeas.DAL
+{
+    public class MovieIdeasContext:DbContext
+    {
+        public MovieIdeasContext() : base("MovieIdeasContext") {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Theater> Theaters { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
